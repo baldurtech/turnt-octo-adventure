fdfbe9ecd93440245c6643a609a265c69bc51d9f
Baldur <master@baldurtech.com>
2014-08-19 14:49:13 +0800

    修改测试，toRealUri

--- src/test/java/com/baldurtech/turnt/octo/adventure/action/ActionTest.java
+++ src/test/java/com/baldurtech/turnt/octo/adventure/action/ActionTest.java
@@ -22,16 +22,16 @@ public class ActionTest extends MiniatureSpiceTestCase {
         request = mock(HttpServletRequest.class);
         response = mock(HttpServletResponse.class);
 
-        when(request.getContextPath()).thenReturn("");
+        when(request.getContextPath()).thenReturn("/calabash");
 
         action = new ContactAction(servletContext, request, response);
     }
 
     public void test_toRealUri_不带参数的actionUri() {
-        assertEquals("/contact/list.do", action.toRealUri("contact/list"));
+        assertEquals("/calabash/contact/list.do", action.toRealUri("contact/list"));
     }
 
     public void test_toRealUri_contact_show带参数的actionUri() {
-        assertEquals("/contact/show.do?id=1", action.toRealUri("contact/show?id=1"));
+        assertEquals("/calabash/contact/show.do?id=1", action.toRealUri("contact/show?id=1"));
     }
 }
