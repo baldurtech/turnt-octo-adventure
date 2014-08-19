a76ae48bc4ceb8eab22ad092c58558b0a1496456
Baldur <master@baldurtech.com>
2014-08-19 16:12:46 +0800

    添加测试：forwardAction跳转到对应的action上，然后把数据都放入request中

--- src/test/java/com/baldurtech/turnt/octo/adventure/action/ActionTest.java
+++ src/test/java/com/baldurtech/turnt/octo/adventure/action/ActionTest.java
@@ -3,6 +3,7 @@ package com.baldurtech.turnt.octo.adventure.action;
 import junit.framework.*;
 import static org.mockito.Mockito.*;
 
+import java.util.Map;
 import java.util.HashMap;
 import java.io.IOException;
 import javax.servlet.ServletException;
@@ -51,6 +52,24 @@ public class ActionTest extends TestCase {
 
         verify(request, never()).setAttribute(anyString(), anyObject());
         verify(servletContext).getRequestDispatcher("/contact/list.do");
+        verify(requestDispatcher).forward(request, response);
+    }
+
+    public void test_forwardAction_转到对应的do上并且把数据放入request() throws Exception {
+        RequestDispatcher requestDispatcher = mock(RequestDispatcher.class);
+        when(servletContext.getRequestDispatcher(anyString())).thenReturn(requestDispatcher);
+
+        Map<String, Object> data = new HashMap<String, Object>(){{
+                put("name", "Tom");
+                put("jobLevel", 5);
+                put("vpmn", "123123");
+            }};
+        action.forwardAction("contact", data);
+
+        verify(request).setAttribute("name", "Tom");
+        verify(request).setAttribute("jobLevel", 5);
+        verify(request).setAttribute("vpmn", "123123");
+        verify(servletContext).getRequestDispatcher("/contact.do");
         verify(requestDispatcher).forward(request, response);
     }
 }
